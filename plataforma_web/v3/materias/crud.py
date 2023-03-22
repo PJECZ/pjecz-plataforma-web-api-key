8e15274d1e2c8865ed8970ee792396811d2d5b1d
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2023-03-21 19:47:37 -0600

    Materias y Materias-Tipos de Juicios

---
+++ plataforma_web/v3/materias/crud.py
@@ -0,0 +1,24 @@
+"""
+Materias v3, CRUD (create, read, update, and delete)
+"""
+from typing import Any
+from sqlalchemy.orm import Session
+
+from lib.exceptions import MyIsDeletedError, MyNotExistsError
+
+from ...core.materias.models import Materia
+
+
+def get_materias(db: Session) -> Any:
+    """Consultar las materias activas"""
+    return db.query(Materia).filter_by(estatus="A").order_by(Materia.id)
+
+
+def get_materia(db: Session, materia_id: int) -> Materia:
+    """Consultar una materia por su id"""
+    materia = db.query(Materia).get(materia_id)
+    if materia is None:
+        raise MyNotExistsError("No existe ese materia")
+    if materia.estatus != "A":
+        raise MyIsDeletedError("No es activo ese materia, está eliminado")
+    return materia
---
+++ plataforma_web/v3/materias_tipos_juicios/paths.py
@@ -0,0 +1,48 @@
+"""
+Materias-Tipos de Juicios v3, rutas (paths)
+"""
+from fastapi import APIRouter, HTTPException, status
+from fastapi_pagination.ext.sqlalchemy import paginate
+
+from lib.database import DatabaseSession
+from lib.exceptions import MyAnyError
+from lib.fastapi_pagination_custom_page import CustomPage, custom_page_success_false
+
+from ...core.permisos.models import Permiso
+from ..usuarios.authentications import CurrentUser
+
+from .crud import get_materias_tipos_juicios, get_materia_tipo_juicio
+from .schemas import MateriaTipoJuicioOut, OneMateriaTipoJuicioOut
+
+materias_tipos_juicios = APIRouter(prefix="/v3/materias_tipos_juicios", tags=["categoria"])
+
+
+@materias_tipos_juicios.get("", response_model=CustomPage[MateriaTipoJuicioOut])
+async def listado_materias_tipos_juicios(
+    current_user: CurrentUser,
+    db: DatabaseSession,
+):
+    """Listado de materias-tipos de juicios"""
+    if current_user.permissions.get("MATERIAS TIPOS JUICIOS", 0) < Permiso.VER:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Forbidden")
+    try:
+        resultados = get_materias_tipos_juicios(db=db)
+    except MyAnyError as error:
+        return custom_page_success_false(error)
+    return paginate(resultados)
+
+
+@materias_tipos_juicios.get("/{{materia_tipo_juicio:singular}_id}", response_model=OneMateriaTipoJuicioOut)
+async def detalle_materia_tipo_juicio(
+    current_user: CurrentUser,
+    db: DatabaseSession,
+    materia_tipo_juicio_id: int,
+):
+    """Detalle de una materia-tipo de juicio a partir de su id"""
+    if current_user.permissions.get("MATERIAS TIPOS JUICIOS", 0) < Permiso.VER:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Forbidden")
+    try:
+        materia_tipo_juicio = get_materia_tipo_juicio(db=db, materia_tipo_juicio_id=materia_tipo_juicio_id)
+    except MyAnyError as error:
+        return OneMateriaTipoJuicioOut(success=False, message=str(error))
+    return OneMateriaTipoJuicioOut.from_orm(materia_tipo_juicio)
