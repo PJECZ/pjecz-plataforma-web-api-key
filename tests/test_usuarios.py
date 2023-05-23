d60a970d552f5014ffab2aa66f763c8ad3c731b3
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2023-05-23 13:02:02 -0600

    Unit tests para usuarios e inventarios

--- tests/test_usuarios.py
+++ tests/test_usuarios.py
@@ -1,5 +1,5 @@
 """
-Unit test for the API for the path usuarios
+Unit tests for usuarios category
 """
 import os
 import unittest
@@ -15,9 +15,18 @@ class TestUsuarios(unittest.TestCase):
 
     def setUp(self) -> None:
         """Initialize the test case"""
-        self.api_key = os.getenv("API_KEY")
-        self.host = os.getenv("HOST")
-        self.timeout = int(os.getenv("TIMEOUT"))
+        # Load environment variables
+        self.api_key = os.getenv("API_KEY", "")
+        self.host = os.getenv("HOST", "")
+        self.timeout = int(os.getenv("TIMEOUT", "20"))
+        # If any of the environment variables is empty, raise an error
+        if not self.api_key:
+            raise ValueError("API_KEY environment variable is empty")
+        if not self.host:
+            raise ValueError("HOST environment variable is empty")
+        if not self.timeout:
+            raise ValueError("TIMEOUT environment variable is empty")
+        # Return super
         return super().setUp()
 
     def test_get_bitacoras(self):
